b07b071ff4324b7c4926acdb493f6d522fc947d5
yyfl96 <824426552@qq.com>
2021-06-24 19:11:49 +0800

    更新《烈焰中的光与影》和转换程序

---
+++ 风乱/程序/文章格式转换/FormatChange.m
@@ -0,0 +1,27 @@
+clear all
+clc
+text = fileread("Text.txt");
+output = "";
+i = 1;
+while( i <= length(text))
+    if text(i) == 13
+        if flag == 0
+            flag = 1;
+            output = output + " ";
+            output = output + " ";
+            output = output + text(i);
+            output = output + text(i+1);
+            i = i + 1;
+        else
+            i = i + 1;
+        end
+    else
+        flag = 0;
+        output = output + text(i);
+    end
+    i = i + 1;
+end
+
+fid = fopen("output.txt",'w');
+fprintf(fid,"%s",output);
+fclose(fid);
